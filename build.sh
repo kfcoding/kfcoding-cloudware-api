281ce4fdfcbefbbe30947f0e6d973cf97ceadb83
WuShaoling <wu12490@gmail.com>
2018-07-19 18:46:01 +0800

    test 2

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-go build -o CloudwareController main.go
-docker build -t kfcoding.com/cloudware-controller:v3.1 .
-docker save kfcoding.com/cloudware-controller:v3.1> cloudware-controller.tar.gz
-scp cloudware-controller.tar.gz root@node2:/root
-scp cloudware-controller.tar.gz root@node3:/root
